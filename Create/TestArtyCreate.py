de5c91edb4496409fe53848558d7378cb1d34ef1
Ukyo Duong <uduong@google.com>
2014-05-13 08:28:03 -0700

    intial creation of TestArtyCreate.py

---
+++ Create/TestArtyCreate.py
@@ -0,0 +1,35 @@
+'''
+Created on May 13, 2014
+
+@author: ukyo.duong
+'''
+
+
+import unittest
+import time
+from helperRecip.testcase import *
+from helperRecip.Elements import Elements
+from helperRecip.WebdriverUtilities import WebdriverUtilities
+from helperRecip.Helpers import Helpers
+
+
+class TestProgramCreate(WebDriverTestCase):
+
+
+    def testProgramCreate(self):
+        self.testname="TestProgramCreate"
+        self.setup()
+        util = WebdriverUtilities()
+        util.setDriver(self.driver)
+        element = Elements()
+        do = Helpers(self)
+        do.setUtils(util)
+        do.login()
+        last_created_object_link =do.createObject("Program")
+        #do.navigateToObjectAndOpenObjectEditWindow("Program",last_created_object_link)
+        #do.deleteObject()
+
+
+
+if __name__ == "__main__":
+    unittest.main()
