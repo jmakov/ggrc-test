67766ceb36a0f49b48e2b0dd55d6f66839404744
Ukyo Duong <uduong@google.com>
2014-05-21 12:48:36 -0700

    Progress on arty tests.

--- helperRecip/WebdriverUtilities.py
+++ helperRecip/WebdriverUtilities.py
@@ -109,6 +109,8 @@ class WebdriverUtilities(unittest.TestCase):
             return True
         except:
             self.fail("ERROR: Element "+element + " not found in clickOnSave()")
+            
+            
 
     
        
