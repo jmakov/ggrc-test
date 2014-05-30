79335fcecc7b97f0fc8ff980c2b37aa361d28cfe
Ukyo Duong <uduong@google.com>
2014-05-29 17:52:16 -0700

    added in more functions to support smoketest

--- helperRecip/WorkFlowHelper.py
+++ helperRecip/WorkFlowHelper.py
@@ -2,11 +2,11 @@
 Created on May 26, 2014
 
 @author: uduong
-@description This class is created holds all functions related to WorkFlow only.  Elements are also stored locally to the function that use it.
-It's unlikely that elements are used by others when you purposely write it for a specifiec function.  Also, when Elements and Helpers classes 
+@description This class is created to hold all functions related to WorkFlow only.  Elements are also stored locally to the function that uses it.
+It's unlikely that elements are used by others when you purposely write it for the specified function.  Also, when Elements and Helpers classes 
 are getting bigger they become unmanageable.  
 
-
+All public functions have "WF" as a postfix to signify that the function is not from the parent class.
 
 '''
 
@@ -34,10 +34,10 @@ class WorkFlowHelper(Helpers):
 
 
 
-    #@log_time
+    @log_time
     # objecName: Controls or Projects, etc.
     # relevantTo: Program
-    def addObjects(self, objectName, relevantTo, whatItem):
+    def addObjectsWF(self, objectName, relevantTo, whatItem):
         add_object_lk = '//a[@href="#objectSelector" and @data-mapping="objects"]' 
         add_new_rule_lk = '//a[@id="addFilterRule"]'
         object_select_drdn = '//select[@id="objects_type"]' 
@@ -79,7 +79,7 @@ class WorkFlowHelper(Helpers):
 
     @log_time
     # Return true if addTask succeed and not error out
-    def addTask(self, taskName="", selectAll=False): 
+    def addTaskWF(self, taskName="", selectAll=False): 
         add_task_lk = '//a[@href="#objectSelector" and @data-mapping="tasks"]' 
         checkboxAll_chbx = '//input[@id="objectAll"]'
         addSelected_bt = '//a[@id="addSelected"]'
@@ -109,7 +109,7 @@ class WorkFlowHelper(Helpers):
     # Create a new task from a window popped up by "add task".  That means the window must be up already before this 
     # function can be used.
     # To test the Cancel feature, set save=False
-    def createNewTask(self, title, detail_description, save=True):
+    def createNewTaskWF(self, title, detail_description, save=True):
         create_task_bt = '//a[@class="btn btn-add addTaskModal"]'
         summary_title_txtbx = '//input[@id="task-title"]'
         detail_txtbx = '//div[@id="newTask"]//textarea[@id="program_description"]'
@@ -127,7 +127,7 @@ class WorkFlowHelper(Helpers):
                 
     @log_time
     # Return true if addPerson succeed and not error out
-    def addPerson(self, personName="", selectAll=False):
+    def addPersonWF(self, personName="", selectAll=False):
         add_person_lk = '//a[@href="#objectSelector" and @data-mapping="person"]' 
         checkboxAll_chbx = '//input[@id="objectAll"]'
         addSelected_bt = '//a[@id="addSelected"]'
@@ -154,7 +154,7 @@ class WorkFlowHelper(Helpers):
     # Return true if addTaskGroup succeed and not error out    
     # if you want to test the Cancel feature then set save=False
     # date format: "2014_05_27", for test purpose please use current date
-    def addTaskGroup(self, groupName, personName, date, save=True):
+    def addTaskGroupWF(self, groupName, personName, date, save=True):
         group_txtbx = '//input[@id="new_object_name"]'
         search_txtbox = '//input[@id="new_object_name"]'
         save_bt = '//a[@id="addTaskGroup"]'
@@ -195,7 +195,7 @@ class WorkFlowHelper(Helpers):
         return True    
         
     @log_time
-    def startWorkFlow(self, proceed=True):       
+    def startWorkFlowWF(self, proceed=True):       
         proceed_bt = '//a[@id="confirmStartWorkflow"]'
         cancel_bt = '//a[@id="cancelStartWorkflow"]'
         self.util.clickOn(self.startWorkflow_bt)
@@ -207,15 +207,38 @@ class WorkFlowHelper(Helpers):
             
         
     @log_time
-    def stopWorkFlow(self):
+    def stopWorkFlowWF(self):
         self.util.clickOn(self.endWorkflow_bt)
         
+    @log_time
+    def unmapObjectsWF(self, objectName):
+        # TODO add search by title in the future; call _searchStringInTable()
+        unmap_lk = '//div[@id="middle_column"]//div[@class="tier-2-info item-content content-open"]//a[@class="info-action unmap pull-right"]'
+        total_count_label = '//div[@id="middle_column"]/section[@class="widget entities people_widget widget-active"]//span[@class="object_count"]'
+        count = self.util.getTextFromXpathString(total_count_label)
+        first_item = '//div[@id="middle_column"]//section[@class="widget objectives objects_widget widget-active"]//li[@class="tree-item object-top" and @data-index="0"]'
+        
+        self.util.hoverOver(first_item)
+        self.util.clickOn(first_item)
+        self.util.clickOn(unmap_lk)
+        
+    @log_time
+    def unmapTasksWF(self, taskName):
+        # TODO add search by title in the future; call _searchStringInTable()
+        unmap_lk = '//div[@id="middle_column"]//div[@class="tier-2-info item-content content-open"]//a[@class="info-action unmap pull-right"]'
+        total_count_label = '//div[@id="middle_column"]/section[@class="widget entities people_widget widget-active"]//span[@class="object_count"]'
+        count = self.util.getTextFromXpathString(total_count_label)
+        first_item = '//div[@id="middle_column"]//section[@class="widget objectives objects_widget widget-active"]//li[@class="tree-item object-top" and @data-index="0"]'
+        
+        self.util.hoverOver(first_item)
+        self.util.clickOn(first_item)
+        self.util.clickOn(unmap_lk)        
         
            
     @log_time
-    # unmap first item in the list -- from the top
-    def unmapPerson(self, personName):
-        # TODO add search by title in the future
+    # Unmap first item in the list -- from the top
+    def unmapPersonWF(self, personName):
+        # TODO add search by title in the future; call _searchStringInTable()
         
         firstItem = '//div[@id="middle_column"]/section[@class="widget entities people_widget widget-active"]//span[@class="person-holder"]'
         unmap_lk = '//div[@id="middle_column"]//a[@class="info-action unmap pull-right"]'
@@ -226,10 +249,22 @@ class WorkFlowHelper(Helpers):
         self.util.clickOn(firstItem)
         self.util.clickOn(unmap_lk)
         
+    # Search for a specified text int the table, and return TRUE if found
+    def _searchStringInTable(self, element, searchName, tableSize):
+        
+        for x in range[1:tableSize]:
+            name = self.util.getTextFromXpathString(element)
+            
+            if name==searchName:
+                return True
+            else:
+                continue
+            
+        
         
     @log_time
-    # unmap first item in the list -- from the top
-    def deleteTaskGroup(self, groupName):        
+    # Unmap first item in the list -- from the top
+    def deleteTaskGroupWF(self, groupName):        
         # TODO add search by title in the future
         count_xpath = '//div[@id="middle_column"]/section[@class="widget programs task_groups_widget widget-active"]//h2'
         title = '//div[@id="middle_column"]/section[@class="widget programs task_groups_widget widget-active"]//div[@class="tree-title-area"]'
@@ -247,9 +282,12 @@ class WorkFlowHelper(Helpers):
         else:
             return False
 
-    #@log_time
-    # navigate inner panel on WorkFlow, e.g., Objects, Task Groups
-    def navigateToMenuItem(self, fromWhatWorkflow, menuItem):
+    
+
+
+    @log_time
+    # Navigate inner panel on WorkFlow, e.g., Objects, Task Groups
+    def navigateToMenuItemWF(self, fromWhatWorkflow, menuItem):
         workflow_menu  = '//li[@class="programs accordion-group workflow-group"]'
         workflow_items = '//li[@class="programs accordion-group workflow-group"]/ul[@class="sub-level"]/li[INDEX]//span'
         count_xpath = '//li[@class="programs accordion-group workflow-group"]//span[@class="item-count"]'
@@ -266,14 +304,52 @@ class WorkFlowHelper(Helpers):
                 break
             else:
                 continue
-    
+       
+    @log_time
+    # Create a new work flow
+    def createWorkflow(self, wfName="", owner=""):
+        # TODO include more elements testing to support regression automation
         
+        workflow_menu_lk  = '//li[@class="programs accordion-group workflow-group"]'
+        workflow_items_lk = '//li[@class="programs accordion-group workflow-group"]/ul[@class="sub-level"]/li[INDEX]//span'
+        wf_create_new_lk = '//li[@class="programs accordion-group workflow-group"]//li[@class="add-new"]'
         
+        title_txtbx = '//div[@id="editAssessmentStandAlone"]//input[@class="input-block-level required"]'
+        owner_txtbx = '//div[@id="editAssessmentStandAlone"]//input[@name="lead_email"]'
+        save_bt = '//a[@id="saveAssessment"]'
+               
+        self.selectCreateNew()
+       
+        self.util.waitForElementToBePresent(title_txtbx, 8)
+        self.util.inputTextIntoField(wfName, title_txtbx)
+        self.util.inputTextIntoField(owner, owner_txtbx)
+        self.util.clickOn(save_bt)
+           
+     
+    #click on Create New link
+    def selectCreateNewWF(self):
+        wf_create_new_lk = '//li[@class="programs accordion-group workflow-group"]//li[@class="add-new"]'
+      
+    def selectAWorkflowWF(self, workflowName):
+        workflow_items_lk = '//li[@class="programs accordion-group workflow-group"]/ul[@class="sub-level"]/li[INDEX]//span'
+             
+        for index in range[1:self.workflowCountOnHLS()]:
+            str =  self.util.getTextFromXpathString(workflow_items_lk.replace("INDEX", index))
+            if str == workflowName:
+                self.util.clickOn(workflow_items_lk.replace("INDEX", index))
+       
+    def workflowCountOnHLS(self):   
+       wf_count = '//li[@class="programs accordion-group workflow-group"]/a/small/span'
+       
+       return  self.util.getTextFromXpathString(wf_count)
+       
+       
         
     @log_time
-    # Already in WorkFlow window, just want to select different menu item, e.g., Task Groups
-    def selectInnerNavMenuItem(self, menuItem):
+    # Already in WorkFlow window, just want to select different menu item, e.g., Workflow Info, or Task Groups
+    def selectInnerNavMenuItemWF(self, menuItem):
         ul_menu = '//ul[@class="nav internav innernav-arrow program cms_controllers_inner_nav ui-sortable workflow-nav"]'
+        li_WorkflowInfo = ul_menu + '/li[1]'
         li_Objects = ul_menu + '/li[2]'
         li_Tasks = ul_menu + '/li[2]'
         li_People = ul_menu + '/li[2]'
@@ -281,7 +357,9 @@ class WorkFlowHelper(Helpers):
         li_History = ul_menu + '/li[@class="history_object"]'
         li_CurrentCycle = ul_menu + '/li[@class="progress-object  finished"]'
         
-        if menuItem == "Objects":
+        if menuItem == "Workflow Info":
+            self.util.clickOn(li_WorkflowInfo)
+        elif menuItem == "Objects":
             self.util.clickOn(li_Objects)
         elif menuItem == "Tasks":
             self.util.clickOn(li_Tasks)
@@ -293,8 +371,52 @@ class WorkFlowHelper(Helpers):
             self.util.clickOn(li_History)
         elif menuItem == "Current Cycle":
             self.util.clickOn(li_CurrentCycle)
+        
+    
+    
+    @log_time
+    # Clone a specified workflow
+    def cloneWorkflowWF(self, workflowName, title="", owner="", save=True, copyTask=True, copyPeople=True, copyObjects=True, copyTaskGroup=True):
+        self.navigateToMenuItemWF(workflowName, "Work Info")
+        self._cloneWorkflow(title, owner, save, copyTask, copyPeople, copyObjects, copyTaskGroup)
+    
+    # Expand more link and click on Clone Workflow button
+    def _cloneWorkflowWF(self, title="", owner="", save=True, copyTask=True, copyPeople=True, copyObjects=True, copyTaskGroup=True):
+        more_lk = '//a[@class="dropdown-toggle info-edit"]'
+        clone_workflow_bt = '//a[@href="#cloneWorkflow"]'
+        cancel_bt = '//div[@id="cloneWorkflow"]//div[@class="deny-buttons"]/a'
+        save_bt = '//a[@id="cloneWorkflowSave"]'
+        
+        myTitle = '//div[@id="cloneWorkflow"]//input[@name="title"]'
+        myOwner = '//div[@id="cloneWorkflow"]//input[@name="lead_email"]'
+        
+        self.util.clickOn(more_lk)
+        self.util.clickOn(clone_workflow_bt)
+        self.util.waitForElementToBePresent(myTitle, 5)
+        
+        if (title != ""):
+            self.util.inputTextIntoField(title, myTitle)
             
+        if (owner != ""):
+            self.util.inputTextIntoField(owner, myOwner)
+    
+        # by default, all checkboxes are checked
+        if copyTask==False:
+            self.util.clickOn('//div[@id="cloneWorkflow"]//label[1]/input[@type="checkbox"]')
+        elif copyPeople==False:
+            self.util.clickOn('//div[@id="cloneWorkflow"]//label[2]/input[@type="checkbox"]')
+        elif copyObjects==False:
+            self.util.clickOn('//div[@id="cloneWorkflow"]//label[3]/input[@type="checkbox"]')
+        elif copyTaskGroup==False:
+            self.util.clickOn('//div[@id="cloneWorkflow"]//label[4]/input[@type="checkbox"]')
             
+        if save==False:
+            self.util.clickOn(cancel_bt)
+        else:
+            self.util.clickOn(save_bt)
+            
+        
+        
             
             
             
