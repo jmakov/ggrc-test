1a03ee07cf78e20063c0a1166e6bd09dc21dec2e
Jernej Makovšek <jernej.makovsek@gmail.com>
2015-12-24 15:13:10 +0100

    test jenkins2

--- src/lib/page/dashboard.py
+++ src/lib/page/dashboard.py
@@ -10,3 +10,4 @@ from lib.page import widget_bar, header
 class DashboardPage(widget_bar.WidgetBarPage, header.HeaderPage):
     def navigate_to(self):
         self._driver.get(constants.uri.BASE + constants.uri.DASHBOARD)
+
